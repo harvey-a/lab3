18f0ca24763b85ed125616cb35b6a5ef3159b9f5
Harvey <harvey@github.com>
2018-04-16 19:29:06 +0800

    changes

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -1,3 +1,9 @@
+"""
+WSGI config for ecommerce project.
+It exposes the WSGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
 import os
 
 from django.core.wsgi import get_wsgi_application
