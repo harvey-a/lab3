a51c3d68e78a2d50fc2068b9f13f37cf564b78c1
Harvey <harvey@github.com>
2018-04-16 19:30:24 +0800

    changes

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -28,7 +28,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'business.apps.BusinessConfig',
+    'shop.apps.ShopConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
